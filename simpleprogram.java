bf98a22bcbb839f4c0034775095a0d5cd9923c4e
Dhanush Ganta <ashokganta40@gmail.com>
2023-01-10 14:05:53 +0900

    first commit

---
+++ simpleprogram.java
@@ -0,0 +1,13 @@
+package App1;
+
+public class simpleprogram {
+
+	public static void main(String[] args) {
+		simpleprogram s1= new simpleprogram();
+		s1.method1();
+
+	}
+	public void method1() {
+		System.out.println("This is a simple java program");
+	}
+}
